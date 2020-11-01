23156b3ea15f6a47460f227e394dd702cbeeea08
Lukas Margaux <m.lukas@rubika-edu.com>
2020-11-01 21:16:18 +0100

    Proto UI setup village

---
+++ FishingRPG/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public void LeaveVillage()
+    {
+        Debug.Log("Leaving village...");
+    }
+
+    public void OpenMenu(GameObject go)
+    {
+        go.SetActive(true);
+    }
+}
