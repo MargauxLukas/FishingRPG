ff710c52c1aaebf0a1bb5c20ec73bd5b8b1e6fd5
Lukas Margaux <m.lukas@rubika-edu.com>
2020-11-17 20:59:24 +0100

    All gameplay tools sriptables done & tools editors wip

--- FishingRPG/Assets/Scripts/Data/FishyFiche.cs
+++ FishingRPG/Assets/Scripts/Data/FishyFiche.cs
@@ -6,7 +6,6 @@ using UnityEditor;
 [CreateAssetMenu(fileName = "NewFishyFiche", menuName = "BFF Tools/Fishy Fiche", order = 10)]
 public class FishyFiche: ScriptableObject
 {
-    //Temporary values waiting for tsv import
     //[Header("Fish Type")]
     public string ID;
     public Sprite appearance;
