3639f81d9e771b4700f73e27cca6737015be5315
Lukas Margaux <m.lukas@rubika-edu.com>
2020-11-25 20:26:44 +0100

    Artisan ui setup wip
    Stuff & Components texts still missing

---
+++ FishingRPG/Assets/Scripts/Behaviors/ChangeTextColor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeTextColor : MonoBehaviour
+{
+    public void SelectedColor()
+    {
+        gameObject.GetComponent<Text>().color = new Color32(66, 41, 36, 255);
+    }
+    
+    public void DeselectedColor()
+    {
+        gameObject.GetComponent<Text>().color = new Color32(254, 242, 184, 255);
+    }
+}
