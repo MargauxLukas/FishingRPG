9076dabcc256ae1bdcbdcfedeccc83e17cd9f4f9
Alexandre COURTIN <alexandre.courtin.76@gmail.com>
2021-01-05 15:23:03 +0100

    Fixed : Can't move when finish fishing

--- FishingRPG/Assets/Scripts/Managers/FishingRodManager.cs
+++ FishingRPG/Assets/Scripts/Managers/FishingRodManager.cs
@@ -160,7 +160,7 @@ public class FishingRodManager : MonoBehaviour
 
 
         //  /!\ Valeur au pif pour tester, need calcul d'un nombre entre 0f et 1f
-        if(FishManager.instance.isAerial)
+        if(FishManager.instance.isAerial && !FishManager.instance.currentFishBehavior.isDead)
         {
             FishManager.instance.aerialEnterWaterX += currentAxis*0.2f;
             FishManager.instance.UpdateAerial();
