7826ca46e5fb306e6c7dd884d9653fdbb439da9f
Alexandre COURTIN <alexandre.courtin.76@gmail.com>
2021-01-10 18:08:00 +0100

    Need merge with Margaux

--- FishingRPG/Assets/Scripts/Managers/DebugManager.cs
+++ FishingRPG/Assets/Scripts/Managers/DebugManager.cs
@@ -12,10 +12,12 @@ public class DebugManager : MonoBehaviour
     {
         Init();
 
+        /*
 #if UNITY_EDITOR
         QualitySettings.vSyncCount = 0; // VSync must be disabled.
         Application.targetFrameRate = 10;
 #endif
+        */
 
 
     }
