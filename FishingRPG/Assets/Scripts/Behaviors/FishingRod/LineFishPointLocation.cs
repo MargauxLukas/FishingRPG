d57c774b892edaf0b5e1936e495a19a146ac044e
AlexC <alexandre.courtin.76@gmail.com>
2021-01-11 14:22:20 +0100

    push

---
+++ FishingRPG/Assets/Scripts/Behaviors/FishingRod/LineFishPointLocation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LineFishPointLocation : MonoBehaviour
+{
+    public Transform farPoint;     //Bobber
+    public Transform nearPoint;    //Check Water Level Point
+
+    private float r;
+    private float fishPointX;
+    private float fishPointZ;
+    void Update()
+    {
+        r = UtilitiesManager.instance.CalculateR();
+
+        fishPointX = farPoint.position.x + (farPoint.position.x - farPoint.position.x) * r;
+        fishPointZ = farPoint.position.z + (farPoint.position.z - farPoint.position.z) * r;
+    }
+}
---
+++ FishingRPG/Assets/Scripts/Behaviors/FishingRod/LinePlayerPointLocation.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinePlayerPointLocation : MonoBehaviour
+{
+    public Transform upPoint;      //Point le plus haut (Fishing Rod Point for PlayerPoint)
+    public Transform downPoint;    //Point le plus bas (Check Water Level Point)
+
+    private float distance;
+    private float r;
+
+    private float playerPointX;
+    private float playerPointZ;
+
+    private void Update()
+    {
+        r = UtilitiesManager.instance.CalculateR();
+
+        playerPointX = upPoint.position.x + (downPoint.position.x - upPoint.position.x) * r;
+        playerPointZ = upPoint.position.z + (downPoint.position.z - upPoint.position.z) * r;
+    }
+
+    public void CheckDistance()
+    {
+
+    }
+}
