acf3205ac75ae12c245b58be63b23229b7db5475
Leithianîn <m.lukas@rubika-edu.com>
2021-01-18 17:02:17 +0100

    MERDE

---
+++ FishingRPG/Assets/Scripts/Managers/SceneLoading.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoading : MonoBehaviour
+{
+    public int sceneIndex;
+
+    void Start()
+    {
+        StartCoroutine(LoadAsyncOperation(sceneIndex));
+    }
+
+    IEnumerator LoadAsyncOperation(int _scene)
+    {
+        AsyncOperation lvl = SceneManager.LoadSceneAsync(_scene);
+
+        while(lvl.progress < 1)
+        {
+
+        }
+
+        yield return new WaitForEndOfFrame();
+    }
+}
