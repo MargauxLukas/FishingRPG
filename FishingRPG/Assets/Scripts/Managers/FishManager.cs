14de7e1b4b02102fd08c3b99f26cf4814ebe71e0
AlexC <alexandre.courtin.76@gmail.com>
2021-01-21 17:19:20 +0100

    Salopard de script qui me demande de save quand je quite unity de ses mor

--- FishingRPG/Assets/Scripts/Managers/FishManager.cs
+++ FishingRPG/Assets/Scripts/Managers/FishManager.cs
@@ -133,7 +133,6 @@ public class FishManager : MonoBehaviour
         isFelling = true;
         currentFishBehavior.fellingFreeze = true;
 
-        FishingRodManager.instance.fishingRodPivot.GetComponent<Rotate>().FellRotation();
         StartCoroutine(FellingFreeze());
     }
 
