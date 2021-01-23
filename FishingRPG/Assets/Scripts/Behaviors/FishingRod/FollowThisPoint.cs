9ae418f913d1cce3971b26b08729c6238d04bd6b
Alexandre COURTIN <alexandre.courtin.76@gmail.com>
2021-01-23 13:12:28 +0100

    Epaule déplacé

--- FishingRPG/Assets/Scripts/Behaviors/FishingRod/FollowThisPoint.cs
+++ FishingRPG/Assets/Scripts/Behaviors/FishingRod/FollowThisPoint.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class FollowThisPoint : MonoBehaviour
 {
     public Transform pointToFollow;
+    public Transform arm;
 
     // Update is called once per frame
     void Update()
     {
         transform.position = pointToFollow.gameObject.GetComponent<SkinnedMeshRenderer>().bounds.center;
+        //arm.transform.localRotation = new Quaternion(FishingRodManager.instance.fishingRodGameObject.transform.localRotation.x, 0f, 0f, 0f);
     }
 }
