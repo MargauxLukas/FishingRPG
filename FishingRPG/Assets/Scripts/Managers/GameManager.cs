41de5aaefdb009086b5d9ecee9148d454e71100b
AdrienGD <adrien.dourgnon@gmail.com>
2021-01-24 14:19:02 +0100

    Intégration lancement et stop ambiance de hub et d'explo

--- FishingRPG/Assets/Scripts/Managers/GameManager.cs
+++ FishingRPG/Assets/Scripts/Managers/GameManager.cs
@@ -7,7 +7,8 @@ public class GameManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Play Sound -> launch ambiance de l'explo
+        AkSoundEngine.PostEvent("AMBWind", FishManager.instance.currentFish.gameObject);
     }
 
     // Update is called once per frame
