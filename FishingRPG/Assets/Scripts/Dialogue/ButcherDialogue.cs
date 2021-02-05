33049aa87db739371a73f804f704492cc64027e9
Alexandre COURTIN <alexandre.courtin.76@gmail.com>
2021-02-05 15:49:34 +0100

    Dialogue NPC

---
+++ FishingRPG/Assets/Scripts/Dialogue/ButcherDialogue.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButcherDialogue : MonoBehaviour
+{
+    public string[] enterButcherShopDialogue = new string[] {};
+    public string[] leaveButcherShopDialogue = new string[] {};
+}
