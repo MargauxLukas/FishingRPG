a1b4d2e5af87a4f8f48f8ef6a0ceb7f4fde3cce1
Alexandre COURTIN <alexandre.courtin.76@gmail.com>
2021-02-10 01:13:38 +0100

    Bobbing head en dialogue

--- FishingRPG/Assets/Scripts/Behaviors/Player/BobbingHead.cs
+++ FishingRPG/Assets/Scripts/Behaviors/Player/BobbingHead.cs
@@ -33,69 +33,71 @@ public class BobbingHead : MonoBehaviour
     // Update is called once per frame
     void Update() 
     {
-        
-        if ((playerManager.canMove) && (Input.GetAxisRaw("Horizontal")>0.1f || Input.GetAxisRaw("Vertical")>0.1f || Input.GetAxisRaw("Horizontal") < -0.1f || Input.GetAxisRaw("Vertical")<-0.1f))
+        if (!TutoManager.instance.isOnDialogue)
         {
-            normeVector = Mathf.Sqrt(Mathf.Pow(Input.GetAxisRaw("Horizontal"),2) + Mathf.Pow(Input.GetAxisRaw("Vertical"),2));
-            //Player is moving
-            timer += Time.fixedDeltaTime * walkingBobbingSpeed * normeVector;
-
-
-            //--Le bruit de pas va-t-il se jouer ?---//
-            if (oldTimer != 0 && playSound)
+            if ((playerManager.canMove) && (Input.GetAxisRaw("Horizontal") > 0.1f || Input.GetAxisRaw("Vertical") > 0.1f || Input.GetAxisRaw("Horizontal") < -0.1f || Input.GetAxisRaw("Vertical") < -0.1f))
             {
-                if ((Mathf.Cos(timer) > 0 && Mathf.Cos(oldTimer) < 0) || ((Mathf.Cos(timer) < 0 && Mathf.Cos(oldTimer) > 0)))   //si cos(oldTimer) et cos(timer) sont de signes différents
+                normeVector = Mathf.Sqrt(Mathf.Pow(Input.GetAxisRaw("Horizontal"), 2) + Mathf.Pow(Input.GetAxisRaw("Vertical"), 2));
+                //Player is moving
+                timer += Time.fixedDeltaTime * walkingBobbingSpeed * normeVector;
+
+
+                //--Le bruit de pas va-t-il se jouer ?---//
+                if (oldTimer != 0 && playSound)
                 {
-                    //Play Sound
-                    AkSoundEngine.PostEvent("OnCharacterWalk", gameObject);      
-                    
-
-                    string textureName = terrainTextureDetector.GetTerrainTextureAtPlayerPosition();
-
-                    if (textureName == "NewLayer")
+                    if ((Mathf.Cos(timer) > 0 && Mathf.Cos(oldTimer) < 0) || ((Mathf.Cos(timer) < 0 && Mathf.Cos(oldTimer) > 0)))   //si cos(oldTimer) et cos(timer) sont de signes différents
                     {
-                        //Debug.Log("Marche sur Gravier");
-                        //Set Materials to Gravel
-                        AkSoundEngine.SetSwitch("Materials", "Gravel", gameObject);
+                        //Play Sound
+                        AkSoundEngine.PostEvent("OnCharacterWalk", gameObject);
+
+
+                        string textureName = terrainTextureDetector.GetTerrainTextureAtPlayerPosition();
+
+                        if (textureName == "NewLayer")
+                        {
+                            //Debug.Log("Marche sur Gravier");
+                            //Set Materials to Gravel
+                            AkSoundEngine.SetSwitch("Materials", "Gravel", gameObject);
+                        }
+                        else if (textureName == "NewLayer 2")
+                        {
+                            //Debug.Log("Marche sur Rocher");
+                            //Set Materials to Rock
+                            AkSoundEngine.SetSwitch("Materials", "Rock", gameObject);
+                        }
+                        else if (textureName == "NewLayer 3")
+                        {
+                            //Set Materials to Sand
+                            AkSoundEngine.SetSwitch("Materials", "Sand", gameObject);
+                            //Debug.Log("Marche sur sable");
+                        }
+                        else
+                        {
+                            //Set Materials to Sand
+                            AkSoundEngine.SetSwitch("Materials", "Rock", gameObject);
+
+                            //Debug.Log("Nom de layer non reconnu");  //ducoup son par défaut ou pas de son comme vous voulez
+                        }
+
+
+
+
+
                     }
-                    else if (textureName == "NewLayer 2")
-                    {
-                        //Debug.Log("Marche sur Rocher");
-                        //Set Materials to Rock
-                        AkSoundEngine.SetSwitch("Materials", "Rock", gameObject);
-                    }
-                    else if (textureName == "NewLayer 3")
-                    {
-                        //Set Materials to Sand
-                        AkSoundEngine.SetSwitch("Materials", "Sand", gameObject);
-                        //Debug.Log("Marche sur sable");
-                    }
-                    else
-                    {
-                        //Set Materials to Sand
-                        AkSoundEngine.SetSwitch("Materials", "Rock", gameObject);
-        
-                        //Debug.Log("Nom de layer non reconnu");  //ducoup son par défaut ou pas de son comme vous voulez
-                    }
-
-
-
-
-
                 }
+                oldTimer = timer;
+                //---//
+
+                transform.localPosition = new Vector3(defaultPosX + Mathf.Sin(timer) * bobbingAmount * normeVector, defaultPosY + Mathf.Abs(Mathf.Sin(timer)) * 1 * bobbingAmount * normeVector, transform.localPosition.z);
             }
-            oldTimer = timer;
-            //---//
+            else
+            {
+                //Idle
+                timer = 0;
+                oldTimer = 0;
 
-            transform.localPosition = new Vector3(defaultPosX + Mathf.Sin(timer) * bobbingAmount * normeVector, defaultPosY + Mathf.Abs(Mathf.Sin(timer)) *1 * bobbingAmount * normeVector, transform.localPosition.z);
-        }
-        else
-        {
-            //Idle
-            timer = 0;
-            oldTimer = 0;
-
-            transform.localPosition = new Vector3(Mathf.Lerp(transform.localPosition.x, defaultPosX, Time.deltaTime * walkingBobbingSpeed), transform.localPosition.y , transform.localPosition.z);
+                transform.localPosition = new Vector3(Mathf.Lerp(transform.localPosition.x, defaultPosX, Time.deltaTime * walkingBobbingSpeed), transform.localPosition.y, transform.localPosition.z);
+            }
         }
         
     }
